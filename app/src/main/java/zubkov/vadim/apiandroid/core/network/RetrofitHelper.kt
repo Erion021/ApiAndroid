b33cf85ead6c5fa086caf4301b0573837e7e1327
erion <zubvad02@gmail.com>
2023-01-25 18:48:51 +0100

    Carga GET(usuarios) sin emulador funcional

--- app/src/main/java/zubkov/vadim/apiandroid/core/network/RetrofitHelper.kt
+++ app/src/main/java/zubkov/vadim/apiandroid/core/network/RetrofitHelper.kt
@@ -1,9 +1,11 @@
 package zubkov.vadim.apiandroid.core.network
 
+import com.google.android.datatransport.runtime.dagger.Provides
 import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 object RetrofitHelper {
     private val okHttpClient = OkHttpClient.Builder()
@@ -12,8 +14,10 @@ object RetrofitHelper {
         .readTimeout(60, TimeUnit.SECONDS)
         .build()
 
+    @Provides
+    @Singleton
     fun getRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl("http://10.0.2.2:8080")
+        return Retrofit.Builder().baseUrl("http://192.168.95.122:8080")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create()).build()
     }
--- app/src/main/java/zubkov/vadim/apiandroid/nav/Navigation.kt
+++ app/src/main/java/zubkov/vadim/apiandroid/nav/Navigation.kt
@@ -4,8 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import zubkov.vadim.apiandroid.login.ui.LoginScreen
-import zubkov.vadim.apiandroid.login.ui.LoginViewModel
+import zubkov.vadim.apiandroid.core.network.RetrofitHelper
 import zubkov.vadim.apiandroid.register.RegisterScreen
 import zubkov.vadim.monkeyfilm.home.ui.HomeScreen
 import zubkov.vadim.monkeyfilm.home.ui.HomeViewModel
@@ -14,6 +13,7 @@ import zubkov.vadim.monkeyfilm.home.ui.HomeViewModel
 fun Navigation() {
     val homeViewModel = HomeViewModel()
     homeViewModel.devolverLista()
+    //val ApiService: RetrofitHelper = RetrofitHelper.getRetrofit().create(RetrofitHelper::class.java)
     val navigationController = rememberNavController()
     NavHost(navController = navigationController, startDestination = Routes.Register.route) {
         composable(route = Routes.Home.route) {
